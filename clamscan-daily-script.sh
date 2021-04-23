16fd3e43fc84483c5896b694db29cdeb6be3654f
mayns82 <37454790+mayns82@users.noreply.github.com>
2021-04-23 15:34:50 +0200

    Add files via upload

---
+++ clamscan-daily-script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# written by Tomas Nevar (tomas@lisenet.com)
+# 17/01/2014 (dd/mm/yy)
+# copyleft free software
+#
+LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+HOST="$(hostname --long)";
+EMAIL_MSG="Please see the log file attached.";
+EMAIL_FROM="clamav-daily@${HOST}";
+EMAIL_TO="admin@example.com";
+DIRTOSCAN="/home";
+
+# Check for mail installation
+type mail >/dev/null 2>&1 || { echo >&2 "I require mail but it's not installed. Aborting."; exit 1; };
+
+# Update ClamAV database
+echo "Looking for ClamAV database updates...";
+freshclam --quiet;
+
+TODAY=$(date +%u);
+
+if [ "$TODAY" == "6" ];then
+	echo "Starting a full weekend scan.";
+	# be nice to others while scanning the entire root
+	nice -n5 clamscan -ri / --exclude-dir=/sys/ --exclude-dir=/proc/ &>"${LOGFILE}";
+else
+	# total size if we use something like /home/*/public_html for scanning
+	DIRSIZE=$(du -shc $DIRTOSCAN 2>/dev/null| cut -f1 | tail -1)
+	echo -e "Starting a daily scan of ${DIRTOSCAN} directory.\nAmount of data to be scanned is ${DIRSIZE}.";
+	nice -n19 clamscan -ri $DIRTOSCAN &>"${LOGFILE}"
+fi
+
+# get the value of "Infected lines"
+MALWARE=$(tail "${LOGFILE}"|grep Infected|cut -d" " -f3);
+
+# if the value is not equal to zero, send an email with the log file attached
+if [ "${MALWARE}" -ne "0" ]; then
+	echo "${EMAIL_MSG}"|mail -a "${LOGFILE}" -s "ClamAV: Malware Found" -r "${EMAIL_FROM}" "${EMAIL_TO}";
+fi
+
+echo "The script has finished.";
+exit 0;
